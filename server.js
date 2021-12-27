a2c2f87f1dd28a269c80305441cfa83a6e9be9a2
Lisa Stillwell <lisastillwell@bellsouth.net>
2021-12-27 17:15:53 -0500

    #1 added routing skelton to server code

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+// These 2 allows us to handle POST data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET request for index.html
+app.get('/', (req, res) =>
+  console.info("received get request for /")
+);
+
+// GET request for notes.html
+app.get('/notes', (req, res) =>
+console.info("received get request for /notes")
+);
+
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  console.info("received get request for /api/notes")
+
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  console.info("received get request for /api/notes")
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
