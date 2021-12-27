6f783bc2d37caea252c1b9442f9060f70c7af039
Lisa Stillwell <lisastillwell@bellsouth.net>
2021-12-27 17:36:14 -0500

    #2 added code to get and save notes to the db.json file

--- helpers/notesIO.js
+++ helpers/notesIO.js
@@ -1,8 +1,27 @@
+const fs = require('fs');
 
 const getNotes = () => {
+
+    var data = fs.readFileSync('./db/db.json','utf8')
+    
+    if (data != null) {
+        notes = JSON.parse(data);
+    }
+    else {
+        notes = [];
+    }
+
+    return notes;
 }
     
-const saveNotes = (notes) => {
+const saveNote = (notes) => {
+
+    try {
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes)) 
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
 
 module.exports = {
